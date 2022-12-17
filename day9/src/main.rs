d6cb548ec32ba3a09af8f2a8a990fac7594a2220
bplotnick <ben.plotnick@gmail.com>
2022-12-16 20:08:36 -0500

    day9 part2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -30,6 +30,9 @@ fn main() {
     let lines = BufReader::new(file).lines().filter_map(|i| i.ok());
     
     part1_inner(lines);
+    let file = File::open("input.txt").expect("can't find input.txt");
+    let lines = BufReader::new(file).lines().filter_map(|i| i.ok());
+    part2_inner(lines)
 }
 
 pub fn part1_inner<I>(lines: I) -> ()
@@ -88,6 +91,57 @@ where
     return ();
 }
 
+pub fn part2_inner<I>(lines: I) -> ()
+where
+    I: IntoIterator,
+    I::Item: AsRef<str>,
+{
+    let mut locs: Vec<(i32, i32)> = vec![(0,0);10];
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+
+    let moves = lines.into_iter().map(
+                            |l| l.as_ref()
+                            .split_once(' ')
+                            .map(|i| (String::from(i.0), String::from(i.1)))
+                            .unwrap())
+         .map(|(dir, amt)| Move{direction: Direction::from(&dir), amount: amt.parse().unwrap()})
+         .collect::<Vec<Move>>();
+    
+    for m in moves {
+        for _ in 0..m.amount {
+            match m.direction {
+                Direction::Left => locs[0].0 -= 1,
+                Direction::Right => locs[0].0 += 1,
+                Direction::Up => locs[0].1 -= 1,
+                Direction::Down => locs[0].1 += 1,
+            }
+            for i in 1..locs.len() {
+                let diff = (locs[i-1].0 - locs[i].0, locs[i-1].1 - locs[i].1);
+                if diff.0.abs() >=2 || diff.1.abs() >= 2 {
+                    locs[i] = locs[i-1];
+                } else {
+                    continue;
+                }
+
+                if diff.0 == 2 {
+                    locs[i].0 -= 1;
+                } else if diff.0 == -2 {
+                    locs[i].0 += 1;
+                }
+
+                if diff.1 == 2 {
+                    locs[i].1 -= 1;
+                } else if diff.1 == -2 {
+                    locs[i].1 += 1;
+                }
+            }
+            visited.insert(locs[9]);
+        }
+    }
+    println!("{}", visited.len());
+    return ();
+}
+
 #[test]
 fn test_case() {
     let x = "R 4
@@ -98,5 +152,5 @@ R 4
 D 1
 L 5
 R 2".to_string();
-    part1_inner(x.lines());
+    part2_inner(x.lines());
 }
